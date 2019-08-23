36a5870130492437af312f421ec815c379fc554e
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-08-23 13:52:13 +0100

    Remove the front of the deque if the element is not in the current sliding window

--- main/sliding_maximum.rb
+++ main/sliding_maximum.rb
@@ -2,12 +2,17 @@ require_relative '../lib/deque'
 
 def sliding_maximum(k,array)
     deque = Deque.new
-
+    res = []
     for i in (0...array.length) do
-        deque.pushBack(array[i]) if deque.is_empty?
-    end
 
-    
+        
+        deque.pushBack(array[i]) if deque.is_empty?
+
+        if deque.topFront == array[i - (k - 1)]
+            deque.popFront
+        end
+
+        
 end
 
-sliding_maximum(3, [1, 3, 5, 7, 9, 2])
+sliding_maximum(4, [1, 3, 5, 7, 9, 2])
