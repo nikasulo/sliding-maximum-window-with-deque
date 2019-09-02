82e9f4c452db38f0cdb94f7035d0107dece6c545
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-02 17:25:36 +0100

    Fix deque methods

--- main/sliding_maximum.rb
+++ main/sliding_maximum.rb
@@ -24,26 +24,28 @@ def sliding_maximum(k,array)
     deque = Deque.new
     result = []
     for i in (0...array.length) do
+
         
         set_max(deque,array[i]) if deque.is_empty?
         
         max = get_max(deque)
-        remove_max(deque) if !max_in_window?((i - k), array, max, deque)
+        
+       
         
         if max < array[i] 
             remove_max(deque)
             set_max(deque, array[i])
         end
 
+        remove_max(deque) unless max_in_window?((i - k), array, max, deque)
         
-        if deque.topBack > array[i] 
-            deque.pushBack(array[i])
+        if deque.topBack >= array[i] || deque.topBack.nil?
+            deque.pushBack(array[i]) unless deque.topBack > array[i]
         else
             deque.popBack
             deque.pushBack(array[i])
         end
-        
-        
+                
         max = get_max(deque)
         
         if i >= k - 1
@@ -56,4 +58,7 @@ def sliding_maximum(k,array)
 end
 
 p sliding_maximum(4, [9, 3, 5, 1, 7, 10])
-p sliding_maximum(3, [1, 3, 5, 7, 9, 2])
+# p sliding_maximum(3, [1, 3, 5, 7, 9, 2])
+# p sliding_maximum(3, [8, 0, 4, 2, 0, 6, 4, 8, 5, 7])
+# p sliding_maximum(2, [14, 13, 5, 1, 12, 10])
+p sliding_maximum(4, [10, 7, 5, 1, 3, 9])
